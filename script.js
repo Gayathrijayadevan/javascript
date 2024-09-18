cf3c02b324d5a74f46517892f57599594eca6ae6
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-09-18 11:30:03 +0530

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+// a=100
+// alert(a)
+
+// var a
+// alert(a)
+
+// let a=100
+// a=20
+// alert(a)
+
+// const a=10
+// alert(a)
+
+let a=[1,2,3]
+let b={name:'abc'}
+alert(typeof b)
---
+++ scripts.js
@@ -0,0 +1,27 @@
+var a=200
+// var b=100
+// alert(a+b)
+// alert(a-b)
+// alert(a*b)
+// alert(a/b)
+// a++
+// a--
+// alert(a)
+
+// a+=200
+// a-=200
+// a*=200
+// a/=200
+// a%=200
+// alert(a)
+
+// a='20'
+// b=20
+// alert(a!==b)
+
+a='20'
+b=20
+// alert(a==b && a>50)
+// alert(a==b || a>50)
+alert(!a==b)
+
