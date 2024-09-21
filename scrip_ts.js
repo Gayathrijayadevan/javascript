7c9c53b61395ad3f57c2ec563ccf6ff29a546f85
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-09-21 10:59:30 +0530

    js

---
+++ scrip_ts.js
@@ -0,0 +1,35 @@
+// function sample(a,b){
+//     console.log(a,b);
+// }
+// sample(10,[1,2,3])
+
+// data=(a,b)=>{
+//     console.log("js arrow function",a,b);
+// }
+// data(10,20)
+
+// data=function(a,b){
+//     console.log('nameless function',a,b);
+// }
+// data(10,100)
+
+a="welcome to all12"
+b="          HELLO WORLD         "
+
+console.log(a)
+console.log(a[5]);
+console.log(a.length)
+console.log(a.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.trim())
+console.log(b.trimStart(),'abb');
+console.log(b.trimEnd(),'mmk')
+console.log(a.charAt(0))
+console.log(a.charCodeAt(0))
+console.log(a.slice(2,6))
+
+console.log(a.match("to"))
+console.log(a.match("[a-z][0-9]"))
+console.log(a.match("[a-z0-9]"))
+console.log(b.search("HELLO"))
+
