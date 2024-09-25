84f69474b8e5b469bac02399723963767d53908e
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-09-25 11:30:38 +0530

    js

--- work1/scrt.js
+++ work1/scrt.js
@@ -1,6 +1,54 @@
-d=[{name:'jai',age:20,place:'ktm'},{name:'john',age:21,place:'ekm'},{name:'sanu',age:20,place:'ekm'},{name:'hari',age:21,place:'ktm'},{name:'rahul',age:20,place:'ktm'}]
+d=[{id:1,name:'jai',age:20},{id:2,name:'john',age:21},{id:3,name:'sanu',age:20},{id:4,name:'hari',age:21},{id:5,name:'rahul',age:20}]
 
-d.forEach(element => {
-    console.log(element.age)
-    
-});
+function display(){
+    // creating a variable and selecting the table using querySelector
+    const t_var=document.querySelector('#peopletable tbody')
+    // clearing the any existing contain in t_var before entering the new one 
+
+    t_var.innerHTML=''
+    d.forEach(element => {
+        // creating row to store the table data 
+        const t_row=document.createElement('tr')
+
+        // creating a variable to store the table data 
+        const t_id=document.createElement('td')
+        t_id.textContent=element.id
+        t_row.appendChild(t_id)
+
+        const t_name=document.createElement('td')
+        t_name.textContent=element.name
+        t_row.appendChild(t_name)
+
+        const t_age=document.createElement('td')
+        t_age.textContent=element.age
+        t_row.appendChild(t_age)
+
+        
+        t_var.appendChild(t_row)
+    });
+}
+
+// to input data into the table from the submit form 
+// selecting the submit form and adding an eventlistener 
+document.getElementById('submit_form').addEventListener('submit',function(event){
+    // to avoid refreshing
+    event.preventDefault()
+
+    // store the inputed value in variables 
+    const id=parseInt(document.getElementById('id').value)
+    const name=document.getElementById('name').value 
+    const age=parseInt(document.getElementById('age').value)
+
+    // pushing it to d 
+    d.push({id:id,name:name,age:age})
+
+    // to clear the input box after submit btn is clicked 
+    document.getElementById('id').value=''
+    document.getElementById('name').value=''
+    document.getElementById('age').value=''
+
+    // calling the function to see the result on the table 
+    display()
+
+})
+display();
